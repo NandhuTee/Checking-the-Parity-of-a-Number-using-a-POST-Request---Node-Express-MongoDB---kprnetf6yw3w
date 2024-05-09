47e67c9ede2f1b9555411e0fdda37bd5c97e9851
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-05-09 22:45:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-// index.js
-
 const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
-    {
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
